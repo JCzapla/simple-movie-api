d1285bcca1ecadcc299b4de257b6133dc809213f
Jedrzej Czapla <jczapla41@gmail.com>
2018-12-04 16:17:29 +0000

    adding remote db

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/", routes);
 app.set("view engine", "ejs");
 
-mongoose.connect("mongodb://localhost/movie_api");
-
+//mongoose.connect("mongodb://localhost/movie_api");
+mongoose.connect("mongodb://jczapla:movieapipswd8@ds127854.mlab.com:27854/movie_api");
 
 app.listen(process.env.PORT, process.env.IP);
 
